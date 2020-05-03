10646b98665c4db8270e5029e488a315b6719735
= <=>
2020-05-03 20:41:50 -0300

    origin

--- application/server.js
+++ application/server.js
@@ -12,7 +12,7 @@ const app = express();
 let server = http.createServer(app);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://herokuapp.com/');
+  res.header('Access-Control-Allow-Origin', 'https://chat-fe-react.herokuapp.com');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
