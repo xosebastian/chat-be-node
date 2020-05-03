9930cbd1a564d400192052a68ba6a4a9838aff82
= <=>
2020-05-03 20:31:19 -0300

    fix

--- application/server.js
+++ application/server.js
@@ -11,7 +11,14 @@ import Logger from "./config/logger.config";
 const app = express();
 let server = http.createServer(app);
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://herokuapp.com/');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(Logger)
